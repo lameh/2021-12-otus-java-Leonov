884b15a6b8fb54fea0aabf4e228ce9199a09bf2d
lameh <lameh@inbox.ru>
2022-03-12 11:50:28 +0300

    Дз с дженериками

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,12 +1,13 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    private final LinkedList<Customer> arrayList = new LinkedList<>();
+    private final Deque<Customer> arrayList = new LinkedList<>();
 
     public void add(Customer customer) {
         arrayList.add(customer);
