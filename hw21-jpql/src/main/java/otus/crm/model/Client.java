d2f1da67ab3935df54897e97356675bfb10ae31f
lameh <lameh@inbox.ru>
2022-05-21 00:27:56 +0300

    ДЗ с jpql

---
+++ hw21-jpql/src/main/java/otus/crm/model/Client.java
@@ -0,0 +1,86 @@
+package otus.crm.model;
+
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, targetEntity = otus.crm.model.Address.class)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true, mappedBy = "client")
+    private List<Phone> phones;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.setPhones(phones);
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name, this.address, this.phones);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Phone> getPhones() {
+        return phones != null ? phones : new ArrayList<>();
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+        if(this.phones != null) {
+            this.phones.forEach(p -> p.setClient(this));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
